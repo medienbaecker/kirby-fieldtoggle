dbf0f002315d6c18a8762aeb52debf83b6e109a8
Thomas Günther <mail@medienbaecker.com>
2016-08-26 18:16:02 +0200

    Added "var" to variables
    Thank you LCD344 in the Kirby forum.

--- fieldtoggle/assets/js/f.js
+++ fieldtoggle/assets/js/f.js
@@ -1,60 +1,56 @@
 $(document).ready(function(){
-  
+
   function fieldtoggle() {
-    
+
     $(".fieldtoggle input:checked").each(function() {
-      
-      field = $(this).parent();
+
+      var field = $(this).parent();
 
       if (field.data("off")) {
-        off = field.data("off").split(" ");
+        var off = field.data("off").split(" ");
       }
       else {
-        off = [];
+        var off = [];
       }
-      
+
       if (field.data("on")) {
-        on = field.data("on").split(" ");
+        var on = field.data("on").split(" ");
       }
       else {
-        on = [];
+        var on = [];
       }
-      
+
       if ( $(this).attr("value") == "true" ) {
         $.each(off, function(key, value) {
           $(".field-name-" + value).closest(".field").hide();
-          console.log("Hide .field-name-" + value);
         });
         $.each(on, function(key, value) {
           $(".field-name-" + value).closest(".field").show();
-          console.log("Show .field-name-" + value);
         });
       }
-      
+
       else if ($(this).attr("value") == "false") {
        $.each(off, function(key, value) {
          $(".field-name-" + value).closest(".field").show();
-         console.log("Show .field-name-" + value);
        });
        $.each(on, function(key, value) {
          $(".field-name-" + value).closest(".field").hide();
-         console.log("Hide .field-name-" + value);
        });
       }
-      
+
     });
-    
+
   }
-  
+
   fieldtoggle();
-  
+
   $("body").on("change", ".fieldtoggle input", function() {
     fieldtoggle();
   });
-  
-    
+
+
   $(document).ajaxComplete(function() {
     fieldtoggle();
   });
-    
+
 });
